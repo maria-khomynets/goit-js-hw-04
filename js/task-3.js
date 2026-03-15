f76864c12e4560281144287f92a7962bd99b8108
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-15 18:02:30 +0200

    task

--- js/task-3.js
+++ js/task-3.js
@@ -15,10 +15,10 @@ const profile = {
   },
 };
 
-console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+console.log(profile.getInfo());
 
 profile.changeUsername('Marco');
-console.log(profile.getInfo()); // "Marco has 300 active hours!"
+console.log(profile.getInfo());
 
 profile.updatePlayTime(20);
-console.log(profile.getInfo()); // "Marco has 320 active hours!"
+console.log(profile.getInfo());
