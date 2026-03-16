796070e240fe15086a8232f99591d16770ed7bf3
Maria Khomynets <masha.khomynets@gmail.com>
2026-03-16 09:26:02 +0200

    в

--- js/task-3.js
+++ js/task-3.js
@@ -22,3 +22,24 @@ console.log(profile.getInfo());
 
 profile.updatePlayTime(20);
 console.log(profile.getInfo());
+
+
+// У змінну pizzaResult записати результат виконання функції makePizza.
+
+// У змінну drinkPointer записати посилання на функцію makeDrink.
+
+// Потім викликати функцію через drinkPointer і записати результат у drinkResult.
+function makePizza() {
+  return "Pizza is ready!";
+}
+
+function makeDrink() {
+  return "Drink is ready!";
+}
+
+const pizzaResult = ;
+const drinkPointer = ;
+const drinkResult = ;
+
+console.log(pizzaResult);
+console.log(drinkResult);
